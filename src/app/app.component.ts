594bdfb34bd3524be71dba58cd8accfecf96e99b
DESKTOP-67C200L\Hugo <kuky85@gmail.com>
2019-01-20 20:55:36 +0100

    - Incluir favicon y etiquetas meta imprescindibles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { SwUpdate, UpdateAvailableEvent } from '@angular/service-worker';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angulargdg';
+    title = 'angulargdg';
+    constructor(
+        private swUpdate: SwUpdate,
+    ) {
+        if (this.swUpdate.isEnabled) {
+            swUpdate.checkForUpdate();
+            this.swUpdate.available.subscribe((event: UpdateAvailableEvent) => {
+                if (confirm(`Do you want to update?`)) {
+                    window.location.reload();
+                }
+            });
+        }
+    }
 }
